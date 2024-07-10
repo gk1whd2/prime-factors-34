4af80e6981efb93fefe5b965df1c773fdd23ce4d
jong2.ha <jong2.ha@samsung.com>
2024-07-10 09:04:11 +0900

    [refactoring] 4

--- prime_factors.py
+++ prime_factors.py
@@ -3,8 +3,10 @@ class PrimeFactors:
     def of(self, value):
         factors = []
         if value == 4:
-            factors.append(2)
-            factors.append(2)
+            while (value % 2 == 0):
+                factors.append(2)
+                value //= 2
+
         elif value > 1:
             factors.append(value)
 
