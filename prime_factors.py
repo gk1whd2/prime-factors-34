de259f48d25d0486375638a802b405e5fdfb0c75
jong2.ha <jong2.ha@samsung.com>
2024-07-10 09:45:47 +0900

    [refactoring Recursive] 5

--- prime_factors.py
+++ prime_factors.py
@@ -14,19 +14,16 @@ class PrimeFactors:
 
     def of_recursive(self, number):
         factors = []
+        dividor = 2
+
         if number == 2:
             factors = [2]
         if number == 3:
             factors = [3]
 
-        if number == 4:
-            if number % 2 == 0:
-                factors.append(2)
-                factors.extend(self.of_recursive(number // 2))
-                number //= 2
-        if number == 6:
-            if number % 2 == 0:
-                factors.append(2)
-                factors.extend(self.of_recursive(number // 2))
-                number //= 2
+        if number > 3:
+            if number % dividor == 0:
+                factors.append(dividor)
+                factors.extend(self.of_recursive(number // dividor))
+                number //= dividor
         return factors
